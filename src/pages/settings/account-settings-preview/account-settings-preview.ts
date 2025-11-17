ed8fd74679a63ccb7baacfe39aad7a27223660db
Mahavir Shah <smahavir@buckman.com>
2025-11-17 20:49:27 +0530

    logout button popup & disconnect button design changes.

--- src/pages/settings/account-settings-preview/account-settings-preview.ts
+++ src/pages/settings/account-settings-preview/account-settings-preview.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 import { AuthService } from '../../../shared/services/auth.service';
 
 import { LoginPage } from '../../onboarding/login/login';
 import { AccountSettingsEditPage } from '../account-settings-edit/account-settings-edit';
 import { Subscription } from 'rxjs';
+import { DevicesService } from 'src/shared/services/devices.service';
 
 @Component({
   selector: 'account-settings-preview',
@@ -17,28 +18,57 @@ export class AccountSettingsPreviewPage {
   private userSubscription: Subscription;
   constructor(
     public navCtrl: NavController,
-    public authService: AuthService
+    public authService: AuthService,
+    public devicesService: DevicesService,
+    private alertController: AlertController
   ) {
     this.userSubscription = this.authService.currentUser$.subscribe(user => {
       this.currentUser = user;
     });
   }
 
-  public logout() {
-    this.authService.signOut().subscribe({
-        next: (result) => {
-            console.log('Sign-out successful, navigating...');
-            this.navCtrl.navigateRoot('/login-page');
-        },
-        error: (error) => {
-            console.error('Sign-out failed:', error);
-            // You might still want to navigate even on error
-            // this.navCtrl.navigateRoot('/login-page'); 
-        },
-        complete: () => {
-            console.log('Sign-out stream completed.');
-        }
+  public async logout() {  
+    let message = 'Are you sure you want to logout ?';
+    if (await this?.devicesService?.connectedDevice?.isConnected()) {
+      message = 'Device is connected currently. Are you sure you want to logout ?';
+    }
+    const alert = await this.alertController.create({
+      header: 'Logout Confirmation',
+        cssClass: 'custom-color-alert',
+        message: message, // Corrected grammar slightly
+        buttons: [
+          { 
+            text: 'Yes', 
+            role: 'confirm', 
+            cssClass: 'primary-button',
+            handler: async () => {
+              if (await this?.devicesService?.connectedDevice?.isConnected()) {
+                this?.devicesService?.connectedDevice?.disconnect();
+              }
+              this.authService.signOut().subscribe({
+                  next: (result) => {
+                      console.log('Sign-out successful, navigating...');
+                      this.navCtrl.navigateRoot('/login-page');
+                  },
+                  error: (error) => {
+                      console.error('Sign-out failed:', error);
+                      // You might still want to navigate even on error
+                      // this.navCtrl.navigateRoot('/login-page'); 
+                  },
+                  complete: () => {
+                      console.log('Sign-out stream completed.');
+                  }
+              });
+            }
+          },
+          { 
+            text: 'No', 
+            role: 'cancel',
+            cssClass: 'primary-button'
+          }
+        ]
     });
+    await alert.present();
   }
 
   public goToAccountSettingsEditPage() {
